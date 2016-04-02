b64f00f1eb1448405453fb2a78b8fddb6029e41e
Azizur Rahman <azizur@prodevstudio.com>
2016-04-02 02:07:10 +0100

    Setup pre and post nam install script to manage bootstrap sass dependency

---
+++ _tools/npm-postinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p _assets/_sass/vendor/bootstrap;
+cp -r node_modules/bootstrap/scss/ _assets/_sass/vendor/bootstrap/;
---
+++ _tools/npm-preinstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf _assets/_sass/vendor/;
