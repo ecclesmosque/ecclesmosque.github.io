390250563a666ef2e5ec3e85d8ec8c38c47c3837
Azizur Rahman <azizur@users.noreply.github.com>
2024-06-01 01:22:33 +0100

    fix: use Prettier as code formatter

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
+  singleAttributePerLine: false,
+};
